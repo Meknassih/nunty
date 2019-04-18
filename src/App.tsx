0d3fe52593cb91051db64d9fb5f9f807191aa554
Hamza El Meknassi <elmeknassi.h@gmail.com>
2019-04-18 12:48:02 +0200

    Changed from JS to TSX Added navbar in Header

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,14 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import './bootstrap-lux.min.css'
 import './App.css';
+import Header from './components/Header';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
+        <Header></Header>
         <header className="App-header">
           <img src={logo} className="App-logo" alt="logo" />
           <p>
